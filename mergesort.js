b3e9bbe92d85a235e5bb3f45f8addcf89b7f1417
davidgonzalezfx <dvdramos16@gmail.com>
2019-12-05 21:10:48 -0500

    After 3H I understand how to code merge sort algorithm, it was hard for me :P

---
+++ mergesort.js
@@ -0,0 +1,25 @@
+function merge_sort(arr, mid = arr.length/2) {
+  if (arr.length<2) {
+    return arr
+  }
+  const left = arr.splice(0, mid)
+  return merge(merge_sort(left), merge_sort(arr))
+}
+
+function merge(arrLeft, arrRight) {
+  let sort_array = []
+  while (arrLeft.length && arrRight.length) {
+    if (arrLeft[0] < arrRight[0]) {
+      sort_array.push(arrLeft.shift())
+      sort_array.push(arrRight.shift())
+    }
+    else {
+      sort_array.push(arrRight.shift())
+      sort_array.push(arrLeft.shift())
+    }
+  }
+  return [...sort_array]
+}
+
+let array = [8, 2, 7, 3, 23, 50, 0, 1]
+console.log(`\n-FINAL: ${merge_sort(array)}- - -`);
